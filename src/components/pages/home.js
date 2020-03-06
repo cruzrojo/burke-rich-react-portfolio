56cffb3f8d52a45a8c673e4088567cff208ed3b8
cruzrojo <burkerich@gmail.com>
2020-03-05 18:40:10 -0700

    committing code and not the old code

---
+++ src/components/pages/home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import axios from "axios";
+
+import PortfolioContainer from "../portfolio/portfolio-container"
+
+export default function () {
+    return (
+        <div>
+        <PortfolioContainer />
+        </div>
+    )
+}
