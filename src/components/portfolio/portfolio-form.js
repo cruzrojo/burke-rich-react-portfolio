63396e16a28012f1be6a1115283128e94c79357a
cruzrojo <burkerich@gmail.com>
2020-03-11 20:02:51 -0600

    updated portfolio-manager.js and portfolio-form.js to make them dynamic for edit or creating a new record

--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -140,7 +140,11 @@ export default class PortfolioForm extends Component {
       withCredentials: true
     })
       .then(response => {
-        this.props.handleSuccessfulFormSubmission(response.data.portfolio_item);
+        if (this.state.editMode) {
+          this.props.handleEditFormSubmission();
+        } else {
+          this.props.handleNewFormSubmission(response.data.portfolio_item);
+        }
       
         this.setState({
           name: "",
@@ -150,7 +154,10 @@ export default class PortfolioForm extends Component {
           url: "",
           thumb_image: "",
           banner_image: "",
-          logo: ""
+          logo: "",
+          editMode: false,
+          apiUrl: `https://burkemrich.devcamp.space/portfolio/portfolio_items/${id}`,
+          apiAction: "post"
         });
 
         [this.thumbRef, this.bannerRef, this.logoRef].forEach(ref => {
