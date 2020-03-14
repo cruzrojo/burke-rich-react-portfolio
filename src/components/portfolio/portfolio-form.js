ef4fe463cdb887478106a3f28fe2ac0ba12460d4
cruzrojo <burkerich@gmail.com>
2020-03-14 17:43:57 -0600

    changed the portfolio-form.scss and the portfolio-form.js to allow for editing of all the pucture files.

--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -230,35 +230,49 @@ export default class PortfolioForm extends Component {
           <div className="image-uploaders three-column">
             
             {this.state.thumb_image && this.state.editMode ? (
-              <img src={this.state.thumb_image} alt="Image"/>
+              <div className ="portfolio-manager-image-wrapper">
+                <img src={this.state.thumb_image} alt="Image"/>
+              </div>
               ) : (
-              
-            <DropzoneComponent
-              ref={this.thumbRef}
-              config={this.componentConfig()}
-              djsConfig = {this.djsConfig}
-              eventHandlers={this.handleThumbDrop()}
-              >
+              <DropzoneComponent
+                ref={this.thumbRef}
+                config={this.componentConfig()}
+                djsConfig = {this.djsConfig}
+                eventHandlers={this.handleThumbDrop()}
+                >
                 <div className="dz-message">Thumbnail</div>
               </DropzoneComponent>
-            )}
-            <DropzoneComponent
-              ref={this.bannerRef}
-              config={this.componentConfig()}
-              djsConfig = {this.djsConfig}
-              eventHandlers={this.handleBannerDrop()}
-              >
-                <div className="dz-message">Banner</div>
-              </DropzoneComponent>
+              )}
 
-            <DropzoneComponent
-              ref={this.logoRef}
-              config={this.componentConfig()}
-              djsConfig = {this.djsConfig}
-              eventHandlers={this.handleLogoDrop()}
-              >
-                <div className="dz-message">Logo</div>
+              {this.state.banner_image && this.state.editMode ? (
+              <div className ="portfolio-manager-image-wrapper">
+                <img src={this.state.banner_image} alt="Image"/>
+              </div>
+              ) : (
+              <DropzoneComponent
+                ref={this.bannerRef}
+                config={this.componentConfig()}
+                djsConfig = {this.djsConfig}
+                eventHandlers={this.handleBannerDrop()}
+                >
+                  <div className="dz-message">Banner</div>
               </DropzoneComponent>
+              )}
+
+              {this.state.logo && this.state.editMode ? (
+              <div className ="portfolio-manager-image-wrapper">
+                <img src={this.state.logo} alt="Image"/>
+              </div>
+              ) : (
+              <DropzoneComponent
+                  ref={this.logoRef}
+                  config={this.componentConfig()}
+                  djsConfig = {this.djsConfig}
+                  eventHandlers={this.handleLogoDrop()}
+                  >
+                    <div className="dz-message">Logo</div>
+              </DropzoneComponent>
+              )}
           </div>
 
           <div>
