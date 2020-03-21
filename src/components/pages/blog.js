e44156fd1828783ca72362635e235d5bc7899e2c
cruzrojo <burkerich@gmail.com>
2020-03-21 16:53:45 -0600

    created more functionality in blog.js to create state and call teh api

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -1,9 +1,32 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import axios from 'axios';
 
 class Blog extends Component{
   constructor(){
     super();
+
+    this.state = {
+      blogItems: []
+    }
+
+    this.getBlogItems = this.getBlogItems.bind(this);
+  }
+
+  getBlogItems() {
+    axios.get("https://burkemrich.devcamp.space/portfolio/portfolio_blogs", {
+      withCredentials: true 
+    }).then(response => {
+      this.setState({
+        blogItems: response.data.portfolio_blogs
+      })
+    }).catch(error => {
+      console.log("getBlogItems Error", error);
+    })
+  }
+
+  componentWillMount() {
+    this.getBlogItems();
   }
 
   render() {
