92b47547a71a30cf5ec94f42e2ba3fe686d6359c
cruzrojo <burkerich@gmail.com>
2020-03-26 20:28:39 -0600

    created code to structure the blog post to bring in the content from the axios get call

--- src/components/pages/blog-detail.js
+++ src/components/pages/blog-detail.js
@@ -14,7 +14,9 @@ export default class BlogDetail extends Component {
     getBlogItem() {
         axios.get(`https://burkemrich.devcamp.space/portfolio/portfolio_blogs/${this.state.currentId}`
         ).then(response => {
-            console.log("response", response);
+            this.setState({
+                blogItem: response.data.portfolio_blog
+            });
         }).catch(error => {
             console.log("getBlogItem error", error);
         })
@@ -25,9 +27,19 @@ export default class BlogDetail extends Component {
     }
 
   render() {
+    const {
+        title,
+        content,
+        featured_image_url,
+        blog_status
+    } =this.state.blogItem;
     return (
       <div>
-        <h1>Blog detail</h1>
+        <h1>{title}</h1>
+        <img src={featured_image_url} alt="Blog Post Image"/>
+        <div>
+            {content}
+        </div>
       </div>
     );
   }
