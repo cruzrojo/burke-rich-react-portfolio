e9c0af023685ebe111b22aeb297c295205d90f03
cruzrojo <burkerich@gmail.com>
2020-08-13 20:08:24 -0600

    added the functionality to display the portfolio item for us

--- src/components/portfolio/portfolio-item.js
+++ src/components/portfolio/portfolio-item.js
@@ -21,31 +21,33 @@ export default class PortfolioItem extends Component {
   render() {
     const { id, name, description, thumb_image_url, logo_url } = this.props.item;
     return (
-      <div
-        className="portfolio-item-wrapper"
-        onMouseEnter={() => this.handleMouseEnter()}
-        onMouseLeave={() => this.handleMouseLeave()}
-      >
+      <Link to={`/portfolio/${id}`}>
         <div
-          className={
-            "portfolio-img-background " + this.state.portfolioItemClass
-          }
-          style={{
-            backgroundImage: "url(" + thumb_image_url + ")"
-          }}
-        />
+          className="portfolio-item-wrapper"
+          onMouseEnter={() => this.handleMouseEnter()}
+          onMouseLeave={() => this.handleMouseLeave()}
+        >
+          <div
+            className={
+              "portfolio-img-background " + this.state.portfolioItemClass
+            }
+            style={{
+              backgroundImage: "url(" + thumb_image_url + ")"
+            }}
+          />
 
-        <div className="img-text-wrapper">
-          <div className="logo-wrapper">
-            <img src={logo_url} />
-          </div>
-
-          <div className="subtitle">
-            <h2>{name}</h2>
+          <div className="img-text-wrapper">
+            <div className="logo-wrapper">
+              <img src={logo_url} />
             </div>
-          <div className="subtitle">{description}</div>
+
+            <div className="subtitle">
+              <h2>{name}</h2>
+              </div>
+            <div className="subtitle">{description}</div>
+          </div>
         </div>
-      </div>
+      </Link>
     );
   }
 }
